0669fffafa9591f727c5be4fe2b7f414403b1f77
pateltrusha <bhesaniyatrusha@gmail.com>
2017-08-17 15:22:37 -0400

    creared server

--- app/app.js
+++ app/app.js
@@ -1,2 +1,11 @@
-function ad(){
-}
+var http=require('http');
+
+var myServer=http.createServer(function(req,res){
+
+res.writeHead(200,{"Content-Type":"text/html"});
+res.write("<h1>Hi Welcome</h1>");
+res.end();
+});
+
+myServer.listen(3000);
+console.log("server started....");
